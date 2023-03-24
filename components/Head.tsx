963dfbda99a5d7d00b962561a0d6c8dc621ba579
michaelslain <misha.slain@icloud.com>
2023-03-23 17:29:13 -0700

    Fixed sizing on mobile

--- components/Head.tsx
+++ components/Head.tsx
@@ -5,7 +5,7 @@ const CustomHead: FC = () => {
     return (
         <Head>
             <title>Timbre</title>
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta
                 name="viewport"
                 content="width=device-width, initial-scale=1.0"
